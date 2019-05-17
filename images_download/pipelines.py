d20b93fe271cf103275cf0e1019cabf3d9badc48
Administrator <416326391@qq.com>
2019-05-17 08:45:00 +0800

    alter

--- images_download/pipelines.py
+++ images_download/pipelines.py
@@ -4,8 +4,16 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-from scrapy.pipelines
+from scrapy.pipelines.images import ImagesPipeline
+from scrapy import Request
 
-class ImagesDownloadPipeline(object):
-    def process_item(self, item, spider):
-        return item
+class ImagesDownloadPipeline(ImagesPipeline):
+    #生成下载的图片的Request
+    def get_media_requests(self, item, info):
+        return [Request(x,meta={"title":item["title"]}) for x in item.get(self.images_urls_field, [])]
+
+    #确定目录以及图片名称
+    def file_path(self, request, response=None, info=None):
+        image_name = request.url.split("/")[-1]
+        title = request.meta["title"]
+        return '%s/%s' % (title,image_name)
